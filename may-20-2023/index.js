0db5d70f85d9d8220c47d5e73fb2fad82c191e79
Asif <abir@dipti.com.bd>
2023-05-20 20:15:15 +0600

    js part 1

---
+++ may-20-2023/index.js
@@ -0,0 +1,92 @@
+var personName = "Kamal Mia"; 
+var personAge = 82; 
+var isAlive = true;
+var personNati = {
+    name: "Aslam mia"
+}
+
+console.log(typeof personName);
+console.log(typeof personAge);
+console.log(typeof isAlive);
+
+console.log(typeof personNati);
+
+// This is a single line comment
+/* 
+This
+is a 
+multiline
+comment
+*/
+
+/**
+ * Arethmetic operator
+ * +
+ * -
+ * *
+ * /
+ * %
+ */
+
+var n1 = 12; 
+var n2 = 6;
+
+/*
+console.log(n1 + n2); 
+console.log(n1 - n2); 
+console.log(n1 * n2); 
+console.log(n1 / n2); 
+console.log(n1 % n2); 
+*/
+
+/**
+ * assignment operator
+ * =
+ * +=
+ * -=
+ * *=
+ * /+
+ * %=
+ */
+
+var n3 = 15;
+n3 += 2;
+n3 -= 4;
+n3 *= 2;
+n3 /= 13;
+n3 %= 2;
+console.log(n3);
+
+/**
+ * ==
+ * ===
+ * <
+ * >
+ * <=
+ * >=
+ * !=
+ * !==
+ */
+
+/**
+ * conditional operator
+ * &&
+ * ||
+ */
+
+/**
+ * increment/decrement operatoe
+ * ++
+ * --
+ */
+
+var a = 40;
+
+if(a !== "40" || a > 18){
+    console.log("You are welcome");
+}else{
+    console.log("Sorry you are not allowed");
+}
+
+
+
