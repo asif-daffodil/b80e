959dd3409aee8307423e8f5f6754dbd7220d9467
Asif <abir@dipti.com.bd>
2023-05-20 18:39:14 +0600

    may 20

---
+++ ct/sobahan.js
@@ -0,0 +1,13 @@
+// mukha bd te ashtese
+/* ekhon jodi apni
+multiline comment korte
+ChannelMergerNode, tahoe.. */
+
+var gaan = "ha le lu jah"; 
+console.log(gaan);
+console.log("It's a song!");
+
+console.log(typeof "Bangladesh");
+console.log(typeof 420);
+console.log(typeof true);
+console.log(typeof null);
